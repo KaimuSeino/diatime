bb2bfbb2f3ea779f400d0c9f2db6964e1820fa34
KaimuSeino <skim07a19yki@icloud.com>
2025-02-08 17:38:45 +0900

    レコーダーページを使用する

---
+++ app/(platform)/recorder/page.tsx
@@ -0,0 +1,9 @@
+const RecoderPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Recorder Page</h1>
+    </div>
+  )
+}
+ 
+export default RecoderPage
